01dfa3262a003573b897e759e32361ed9840dd96
AubreyJohns <aubreyomondi@gmail.com>
2019-07-05 00:07:26 +0300

    added migration add_image_to_positions

--- database/migrations/2019_07_04_210519_add_image_to_positions.php
+++ database/migrations/2019_07_04_210519_add_image_to_positions.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePositionsTable extends Migration
+class AddImageToPositions extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreatePositionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('positions', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('position');
-            $table->timestamps();
+        Schema::table('positions', function($table) {
+            $table->string('image');
         });
     }
 
@@ -27,6 +25,8 @@ class CreatePositionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('positions');
+        Schema::table('positions', function($table) {
+            $table->dropColumn('image');
+        });
     }
 }
