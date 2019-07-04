ca0d428bd7e4eac6a19e425d24ca6ddeb977c6a2
AubreyJohns <aubreyomondi@gmail.com>
2019-07-04 12:03:51 +0300

    changed save

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -30,10 +30,10 @@ class CandidateController extends Controller
     {
         $image = $request->file('image');
         $imageName= $image->getClientOriginalName();
-        $imagePath=public_path('/uploads/');
+        //$imagePath=public_path('/uploads/');
         //$image->move($imagePath,$imageName);
         $image = Image::make($image)->encode('png');
-        $image ->save( $imagePath . $imageName );
+        $image ->save( '../public/uploads/' . $imageName );
         $candidate=Candidate::create(['name' => $request->name,'position' => $request->position,'manifesto' => $request->manifesto,'image' => $imageName]);
 
         return response()->json($candidate);
